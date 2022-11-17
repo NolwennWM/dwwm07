a1a8089c6b9a0fcbda1d41d336db91d6c2a6fceb
NolwennWM <47659634+NolwennWM@users.noreply.github.com>
2022-11-17 15:51:00 +0100

    intersection correction

--- 04-javascript/05-intersection/exercice/script.js
+++ 04-javascript/05-intersection/exercice/script.js
@@ -14,3 +14,49 @@
  * 4. Ajouter la détection du dernier paragraphe au nouveau dernier paragraphe.
  */
 
+const ps = document.querySelectorAll('main p');
+const obs = new IntersectionObserver(showPara, {threshold: 0.5});
+
+ps.forEach(p=>obs.observe(p));
+
+function showPara(entries)
+{
+    // console.log(entries);
+    entries.forEach(reveal)
+}
+function reveal(entry)
+{
+    // console.log(entry);
+    if(entry.isIntersecting)
+    {
+        entry.target.style.opacity= "1";
+        // entry.target.style.transform = "translateX(0)";
+        entry.target.classList.add("roll-in-left");
+        obs.unobserve(entry.target);
+    }
+}
+
+// exo 2 :
+let lastP = document.querySelector('main p:last-child');
+const obsLast = new IntersectionObserver(addMore, {rootMargin: "200px"});
+const m = document.querySelector('main');
+
+obsLast.observe(lastP);
+
+function addMore(entries)
+{
+    let ent = entries[0];
+    if(ent.isIntersecting)
+    {
+        let p;
+        for (let i = 0; i < 10; i++) {
+            p = document.createElement("p");
+            p.textContent = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut obcaecati debitis cupiditate, perspiciatis aut omnis enim commodi unde, in quis nobis nostrum corporis quam dolores qui ducimus ex voluptate eveniet.";
+            m.append(p);
+            obs.observe(p);
+        }
+        obsLast.observe(p);
+        obsLast.unobserve(lastP);
+        lastP = p;
+    }
+}
