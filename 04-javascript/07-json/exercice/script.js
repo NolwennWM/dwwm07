4bf0080091d1b9820421eb954a8df2aecc6efed2
Nolwenn <nolwenn.wm@afci-formation.fr>
2022-11-22 10:13:10 +0100

    cours sur JSON + énoncé exercice

---
+++ 04-javascript/07-json/exercice/script.js
@@ -0,0 +1,12 @@
+/**
+ * 1. Créer une todo list. à chaque appui sur le bouton ajout,
+ * créer un nouvel élément dans la liste.
+ * cet élément doit contenir la valeur de l'input et une croix.
+ * On en profitera pour vider l'input.
+ * 2. le clique sur un élément de la liste lui ajoutera une classe qui aura pour 
+ * effet de barrer l'élément.
+ * 3. le clique sur la croix supprimera l'élément concerné.
+ * 4. sauvegarder la liste en localstorage.
+ * 5. afficher la liste sauvegardé au chargement de la page.
+ * 6. éditer la liste lorsque l'on coche ou supprime un élément.
+ */
---
+++ 04-javascript/07-json/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", saveData);
+
+function saveData(e)
+{
+    // J'empêche le chargement de page lors de la soumission du formulaire
+    e.preventDefault();
+    // Je transfère les données de mon formulaire à l'objet FormData.
+    const data = new FormData(form);
+    // Je déclare un objet vide
+    let user = {};
+    // un forEach sur data me permet de récupérer les valeurs et noms de mes champs.
+    data.forEach((value, name)=>{
+        // console.log(value, name);
+        user[name] = value;
+    })
+    // console.log(user);
+    showUser(user);
+    // JSON.stringify permet de transformer un objet ou tableau en string JSON.
+    const strUser = JSON.stringify(user);
+    localStorage.setItem("user", strUser);
+}
+function showUser(u)
+{
+    const h1 = document.querySelector("h1");
+    h1.textContent = `Je suis ${u.prenom}, ${u.age} ans !`;
+}
+
+const userString = localStorage.getItem("user");
+if(userString)
+{
+    // console.log(userString);
+    // JSON.parse() permet de transformer un string JSON en objet ou tableau javascript.
+    const user = JSON.parse(userString);
+    // console.log(user);
+    showUser(user);
+}
